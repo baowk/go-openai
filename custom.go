07f821afaf526932e6eaaab3424f9fb7f2732b58
baowk <baowk1024@gmail.com>
2024-11-23 10:30:05 +0800

    增加自定义处理

--- custom.go
+++ custom.go
@@ -7,6 +7,39 @@ import (
 	"strings"
 )
 
+func (c *Client) CreateChatCompletionCustom(
+	ctx context.Context,
+	request ChatCompletionRequest, urlSuffix string,
+) (response ChatCompletionResponse, err error) {
+	if request.Stream {
+		err = ErrChatCompletionStreamNotSupported
+		return
+	}
+
+	// urlSuffix := chatCompletionsSuffix
+	// if !checkEndpointSupportsModel(urlSuffix, request.Model) {
+	// 	err = ErrChatCompletionInvalidModel
+	// 	return
+	// }
+
+	// if err = validateRequestForO1Models(request); err != nil {
+	// 	return
+	// }
+
+	req, err := c.newRequest(
+		ctx,
+		http.MethodPost,
+		c.fullURLSimple(urlSuffix),
+		withBody(request),
+	)
+	if err != nil {
+		return
+	}
+
+	err = c.sendRequest(req, &response)
+	return
+}
+
 func (c *Client) CreateCompletionCustom(ctx context.Context,
 	request CompletionRequest, urlSuffix string) (response CompletionResponse, err error) {
 	if request.Stream {
